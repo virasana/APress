0bb3d2350399e58aee21e4a39f257fe6546e2c57
virasana <jeanpierrefouche@gmail.com>
2015-11-10 23:25:30 +0000

    added missing files

---
+++ Chapter 02/PartyInvites/PartyInvites/Controllers/RsvpController.cs
@@ -0,0 +1,18 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PartyInvites.Controllesr
+{
+    public class RsvpController : ApiController
+    {
+        public IEnumerable<GuestResponse> ListAttendees()
+        {
+            return Repository.Responses.Where(x => x.WillAttend == true);
+        }
+    }
+}
